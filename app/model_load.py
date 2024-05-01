a4c1883801236a963ed836fd4fd4dfa5707b721a
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-02 02:35:46 +0530

    unsloth test file addition

--- app/model_load.py
+++ app/model_load.py
@@ -5,8 +5,15 @@ import transformers
 from unsloth import FastLanguageModel
 from paddleocr import PaddleOCR
 
-model_id = "/home/hera/workspace/llama3/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
+HF_TOKEN= "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"
+#model_id = "/home/hera/workspace/llama3/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
+#unsloth_model = "/home/hera/workspace/llama3/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
 
+model_id= "/data/models/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
+unsloth_model = "/data/models/models--unsloth--llama-3-8b-Instruct-bnb-4bit/snapshots/efa44c86af4fcbbc3d75e6cb1c8bfaf7f5c7cfc1"
+
+# model_id= "meta-llama/Meta-Llama-3-8B-Instruct"
+# unsloth_model = "unsloth/llama-3-8b-Instruct"
 
 class ModelLoad:
     def __init__(self):
@@ -45,7 +52,7 @@ class ModelLoad:
             dtype = None
             load_in_4bit = True
             model, tokenizer = FastLanguageModel.from_pretrained(
-                model_name="unsloth/llama-3-8b-Instruct",
+                model_name=unsloth_model,
                 max_seq_length=max_seq_length,
                 dtype=dtype,
                 load_in_4bit=load_in_4bit
