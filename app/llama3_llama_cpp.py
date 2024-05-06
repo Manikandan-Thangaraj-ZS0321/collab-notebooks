6e6e7f2a9869cb2a29d70aad51d2a022a267b8bd
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-06 11:33:27 +0530

    added llama cpp version

---
+++ app/llama3_llama_cpp.py
@@ -0,0 +1,80 @@
+import torch
+import gc
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from model_load import ModelLoad
+
+pipeline = ModelLoad.krypton_chat_llamacpp_model_load()
+app = FastAPI()
+paddle_ocr = ModelLoad.paddleocr_model_load()
+
+
+class LlamaRequest(BaseModel):
+    inputFilePath: str
+    promptFilePath: str
+
+
+def get_file_content(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+        return text
+
+
+def generate_tokens_paddle(image_path: str) -> str:
+    try:
+        result_paddle = paddle_ocr.ocr(image_path, cls=True)
+        extracted_text = ""
+        for result in result_paddle:
+            for record in result:
+                txt = record[1][0]
+                extracted_text += txt + "\n"
+        return extracted_text
+    except Exception as e:
+        raise e
+
+
+@app.post("/chat/krypton")
+def read_item(request: LlamaRequest):
+    try:
+        ocr_result = generate_tokens_paddle(request.inputFilePath)
+        prompt_val = get_file_content(request.promptFilePath)
+        messages = [
+            {"role": "system", "content": prompt_val},
+            {"role": "user", "content": ocr_result},
+        ]
+
+        prompt = pipeline.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+
+        terminators = [
+            pipeline.tokenizer.eos_token_id,
+            pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+
+        output = pipeline(
+            prompt,  # Prompt
+            max_tokens=512,  # Generate up to 512 tokens
+            stop=["</s>"],
+            # Example stop token - not necessarily correct for this specific model! Please check before using.
+            echo=True  # Whether to echo the prompt
+        )
+        print(output)
+
+        # prompt_result = outputs[0]["generated_text"][len(prompt):]
+        prompt_result = output
+        return prompt_result
+
+    except Exception as ex:
+        raise ex
+    finally:
+        gc.collect()
+        torch.cuda.empty_cache()
+
+
+@app.post("/argon/text")
+def text_extraction_by_paddle(image_path: str):
+    return generate_tokens_paddle(image_path)
--- app/model_load.py
+++ app/model_load.py
@@ -5,6 +5,7 @@ import os
 
 from unsloth import FastLanguageModel
 from paddleocr import PaddleOCR
+from llama_cpp import Llama
 
 HF_TOKEN = os.environ['HF_TOKEN']
 #model_id = "/home/hera/workspace/llama3/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
@@ -13,8 +14,9 @@ HF_TOKEN = os.environ['HF_TOKEN']
 # model_id= "/data/models/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/e5e23bbe8e749ef0efcf16cad411a7d23bd23298"
 # unsloth_model = "/data/models/models--unsloth--llama-3-8b-Instruct-bnb-4bit/snapshots/efa44c86af4fcbbc3d75e6cb1c8bfaf7f5c7cfc1"
 
-model_id= "meta-llama/Meta-Llama-3-8B-Instruct"
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 unsloth_model = "unsloth/llama-3-8b-Instruct"
+llama_cpp_model = "QuantFactory/Meta-Llama-3-8B-Instruct-GGUF"
 
 class ModelLoad:
     def __init__(self):
@@ -62,6 +64,26 @@ class ModelLoad:
             )
             logging.info("The krypton chat model has been successfully loaded")
             return model, tokenizer
+        except Exception as ex:
+            logging.error("Error in loading krypton chat model")
+            raise ex
+
+    @staticmethod
+    def krypton_chat_llamacpp_model_load():
+
+        try:
+            max_seq_length = 2048
+            dtype = None
+            load_in_4bit = True
+            llm = Llama(
+                model_path=llama_cpp_model,  # Download the model file first
+                n_ctx=max_seq_length,
+                # The max sequence length to use - note that longer sequence lengths require much more resources
+                n_threads=8,  # The number of CPU threads to use, tailor to your system and the resulting performance
+                n_gpu_layers=1  # The number of layers to offload to GPU, if you have GPU acceleration available
+            )
+            logging.info("The krypton chat model has been successfully loaded")
+            return llm
         except Exception as ex:
             logging.error("Error in loading krypton chat model")
             raise ex
