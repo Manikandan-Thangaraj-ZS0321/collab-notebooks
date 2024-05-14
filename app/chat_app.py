cfcea794c911265ae4ecfd7c6de89d517eda71c5
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-15 00:28:15 +0530

    streamlit app

--- app/chat_app.py
+++ app/chat_app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 import requests
-from llama_cpp_model_load import ModelLoader
-import torch
-import gc
-from logger_handler import logger
-from llama3_llama_cpp import ModelUserClass
-
-model_loader = ModelLoader()
-pipeline_model = ModelUserClass(model_loader)
-
 
 st.title("Intics Chatbot")
 
@@ -21,32 +12,6 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-def chat_prompt(prompt: str):
-    try:
-        prompt_val = prompt
-
-        messages = [
-            {"role": "system", "content": prompt_val},
-            {"role": "user", "content": ""},
-        ]
-
-        # response = pipeline.create_chat_completion(messages=messages, response_format={"type": "json_object"})
-        response = pipeline_model.use_model().create_chat_completion(messages=messages)
-
-        prompt_result = response["choices"][0]["message"]["content"].strip()
-        # prompt_result = response
-
-        logger.info("completed response generation from llm")
-
-        return prompt_result
-
-    except Exception as ex:
-        raise ex
-    finally:
-        gc.collect()
-        torch.cuda.empty_cache()
-
-
 if instruction := st.chat_input("Ask me..."):
 
     st.session_state.messages.append({"role": "user", "content": instruction})
@@ -54,12 +19,11 @@ if instruction := st.chat_input("Ask me..."):
     with st.chat_message("user"):
         st.markdown(instruction)
 
-    # url = 'http://192.168.10.238:10002/chat/llama'
-    # headers = {'accept': 'application/json'}
-    # with st.chat_message("assistant"):
-    #     response = requests.post(url, headers=headers, params={'prompt': instruction})
-    #     prompt_result = response.
-    #     st.write(prompt_result)
+    url = 'http://192.168.10.238:10002/chat/llama'
+    headers = {'accept': 'application/json'}
+    with st.chat_message("assistant"):
+        response = requests.post(url, headers=headers, params={'prompt': instruction})
+        prompt_result = response.text
+        st.write(prompt_result)
 
-    prompt_result = chat_prompt(instruction)
     st.session_state.messages.append({"role": "assistant", "content": prompt_result})
--- app/llama3_llama_cpp.py
+++ app/llama3_llama_cpp.py
@@ -3,29 +3,14 @@ import gc
 import os
 import json
 
-from fastapi import FastAPI, File, UploadFile, Query
-from fastapi.responses import StreamingResponse
-
 from pydantic import BaseModel
 from fastapi import FastAPI
-from llama_cpp_model_load import ModelLoader
+from model_load import ModelLoad
 from text_extraction import TextExtraction
 from logger_handler import logger
 from typing import List
 
-
-class ModelUserClass:
-    def __init__(self, model_loader):  # Inject the model loader
-        self.model_loader = model_loader
-
-    def use_model(self):
-        # Get the model from the provided loader
-        return self.model_loader.get_model()
-
-
-model_loader = ModelLoader()
-pipeline_model = ModelUserClass(model_loader)
-
+pipeline = ModelLoad.krypton_chat_llamacpp_model_load()
 text_argon_model = TextExtraction.argon_text_model_load()
 text_xenon_model = TextExtraction.xenon_text_model_load()
 processor, text_krypton_model = TextExtraction.krypton_text_model_load()
@@ -46,80 +31,13 @@ class LlamaRequest(BaseModel):
     textExtractionModel: str
 
 
-class ChatRequest(BaseModel):
-    inputFilePath: str
-    textExtractionModel: str
-
-
 def get_file_content(file_path):
     with open(file_path, 'r') as file:
         text = file.read()
         return text
 
 
-@app.post("/chat/llama/files")
-def chat_with_files(request: ChatRequest, prompt: str = Query(...)):
-    try:
-        text_extraction_model = request.textExtractionModel
-
-        if text_extraction_model == "ARGON":
-            ocr_result = TextExtraction.text_extraction_argon(request.inputFilePath, text_argon_model)
-        elif text_extraction_model == "KRYPTON":
-            ocr_result = TextExtraction.text_extraction_krypton(request.inputFilePath, processor, text_krypton_model)
-        else:
-            ocr_result = TextExtraction.text_extraction_xenon(request.inputFilePath, text_xenon_model)
-
-        prompt_val = prompt
-
-        messages = [
-            {"role": "system", "content": prompt_val},
-            {"role": "user", "content": ocr_result},
-        ]
-
-        # response = pipeline.create_chat_completion(messages=messages, response_format={"type": "json_object"})
-        response = pipeline_model.use_model().create_chat_completion(messages=messages, stream=True)
-
-        prompt_result = response["choices"][0]["message"]["content"].strip()
-        # prompt_result = response
-
-        logger.info("completed response generation from llm")
-
-        return prompt_result
-
-    except Exception as ex:
-        raise ex
-    finally:
-        gc.collect()
-        torch.cuda.empty_cache()
-
-
-@app.post("/chat/llama")
-def chat_prompt(prompt: str = Query(...)):
-    try:
-        prompt_val = prompt
-
-        messages = [
-            {"role": "system", "content": prompt_val},
-            {"role": "user", "content": ""},
-        ]
-
-        # response = pipeline.create_chat_completion(messages=messages, response_format={"type": "json_object"})
-        response = pipeline_model.use_model().create_chat_completion(messages=messages)
-
-        prompt_result = response["choices"][0]["message"]["content"].strip()
-        # prompt_result = response
-
-        logger.info("completed response generation from llm")
-
-        return prompt_result
-
-    except Exception as ex:
-        raise ex
-    finally:
-        gc.collect()
-        torch.cuda.empty_cache()
-
-
+@app.post("/chat/krypton")
 def process_file(request: LlamaRequest):
     try:
         text_extraction_model = request.textExtractionModel
@@ -139,7 +57,7 @@ def process_file(request: LlamaRequest):
         ]
 
         # response = pipeline.create_chat_completion(messages=messages, response_format={"type": "json_object"})
-        response = pipeline_model.use_model().create_chat_completion(messages=messages)
+        response = pipeline.create_chat_completion(messages=messages)
 
         prompt_result = response["choices"][0]["message"]["content"].strip()
         # prompt_result = response
@@ -210,27 +128,3 @@ def text_extraction_by_xenon(image_path: str):
 @app.post("/krypton/text")
 def text_extraction_by_krypton(image_path: str):
     return TextExtraction.text_extraction_krypton(image_path, processor, text_krypton_model)
-
-
-@app.post("/multipart-upload")
-async def create_papers(file: UploadFile = File(...)):
-    try:
-        outputDir = "/home/hera/data/output"
-        if not os.path.exists(outputDir):
-            os.makedirs(outputDir, exist_ok=True)
-
-        output_file = os.path.join(outputDir, file.filename)
-
-        if os.path.exists(output_file):
-            logger.info("File {} already exists".format(file.filename))
-            return {"message": "File already downloaded", "filename": file.filename}
-
-        with open(output_file, "wb") as f:
-            f.write(file.file.read())
-
-        logger.info("File {} downloaded successfully".format(file.filename))
-        return output_file
-
-    except Exception as e:
-        logger.error("Error in downloading multipart file {} with exception {}".format(file.filename, e))
-        return {"error": str(e)}
