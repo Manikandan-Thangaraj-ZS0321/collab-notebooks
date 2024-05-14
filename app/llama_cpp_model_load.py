7b37cb819bf4394ab681ecf018cf1c6d0232fce5
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-15 00:06:03 +0530

    streamlit app

--- app/llama_cpp_model_load.py
+++ app/llama_cpp_model_load.py
@@ -2,25 +2,14 @@ from model_load import ModelLoad
 
 
 class ModelLoader:
-    """Singleton class to load and manage the model."""
-
-    __instance = None  # Private attribute to store the single instance
-
-    def __new__(cls):
-        """Creates a new instance or returns the existing one."""
-        if not ModelLoader.__instance:
-            ModelLoader.__instance = object.__new__(cls)
-        return ModelLoader.__instance
-
     def __init__(self):
-        """Private constructor to prevent direct object creation."""
-        if self.__instance is not None:
-            raise Exception("Singleton class can't be instantiated directly.")
         # Your model loading logic here
         self.model = self.load_model()
 
     def load_model(self):
-        # ... (Your model loading code)
+        # Replace this with your actual model loading code
+        print("Loading model...")
+        # ... (Your model loading logic using your specific library)
         return ModelLoad.krypton_chat_llamacpp_model_load()
 
     def get_model(self):
