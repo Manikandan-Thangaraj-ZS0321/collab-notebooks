241723baa7385189ab2513c2d57b3bd487b7f4ab
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-14 23:27:49 +0530

    streamlit app

--- app/llama_cpp_model_load.py
+++ app/llama_cpp_model_load.py
@@ -3,7 +3,11 @@ from model_load import ModelLoad
 
 class ModelLoaderClass:
     def __init__(self):
-        self.model = ModelLoad.krypton_chat_llamacpp_model_load()
+        self.model = self.load_model()
+
+    def load_model(self):
+        # ... (Your model loading code)
+        return ModelLoad.krypton_chat_llamacpp_model_load()
 
     def get_loaded_model(self):
         return self.model
