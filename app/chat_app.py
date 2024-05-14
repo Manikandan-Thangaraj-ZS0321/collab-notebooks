7dee9c7edf7f51532be1d6e341678d31297ee6ce
manikandan-tm <manikandan.tm@zucisystems.com>
2024-05-14 16:43:22 +0530

    streamlit app

--- app/chat_app.py
+++ app/chat_app.py
@@ -18,31 +18,30 @@ for message in st.session_state.messages:
 #     with st.chat_message("user"):
 #         st.markdown(prompt)
 
-instruction = st.chat_input("Your message")
 
-messages = [
-    {"role": "system", "content": ""},
-    {"role": "user", "content": instruction},
-]
+if instruction := st.chat_input("Your message"):
+    messages = [
+        {"role": "system", "content": ""},
+        {"role": "user", "content": instruction},
+    ]
+    with st.chat_message("user"):
+        st.markdown(instruction)
 
-with st.chat_message("user"):
-    st.markdown(instruction)
+    st.session_state.messages.append({"role": "user", "content": instruction})
 
-st.session_state.messages.append({"role": "user", "content": instruction})
-
-with st.chat_message("assistant"):
-    # stream = client.chat.completions.create(
-    #     model=st.session_state["openai_model"],
-    #     messages=[
-    #         {"role": m["role"], "content": m["content"]}
-    #         for m in st.session_state.messages
-    #     ],
-    #     stream=True,
-    # )
-    response_val = ModelLoad.krypton_chat_llamacpp_model_load().create_chat_completion(messages=messages, stream=True)
-    # prompt_result = response_val["choices"][0]["message"]["content"].strip()
     with st.chat_message("assistant"):
-        st.markdown(response_val)
+        # stream = client.chat.completions.create(
+        #     model=st.session_state["openai_model"],
+        #     messages=[
+        #         {"role": m["role"], "content": m["content"]}
+        #         for m in st.session_state.messages
+        #     ],
+        #     stream=True,
+        # )
+        response_val = ModelLoad.krypton_chat_llamacpp_model_load().create_chat_completion(messages=messages, stream=True)
+        # prompt_result = response_val["choices"][0]["message"]["content"].strip()
+        with st.chat_message("assistant"):
+            st.markdown(response_val)
 
-    response = st.markdown(response_val)
+        response = st.markdown(response_val)
     st.session_state.messages.append({"role": "assistant", "content": response})
